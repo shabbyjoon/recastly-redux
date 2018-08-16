346d4cf37d0cab4842a0e2d9ee7b193972fa6749
shabbyjoon <samokhtarani@yahoo.com>
2018-08-15 20:30:47 -0700

    completed up to reducers

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,19 +1,13 @@
 import Redux from 'redux';
 
-const StateInit = {
-  videos: [],
-  currentVideo: null
-}
 
 
-var currentVideoReducer = (state = StateInit, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
   //currentvideo is the action object
   switch (action.type) {
     case 'CHANGE_VIDEO':
-      return Object.assign({}, state, {
-        currentVideo: action.video
-      })
+      return action.video
       default:
         return state
   }
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,17 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
+  debugger;
+  if (!action) {
+    throw new Error('pass action');
+  }
+  switch (action.type) {
+    case 'CHANGE_VIDEO_LIST':
+      return action.videos
+      default:
+        return state
+  }
 };
 
 export default videoListReducer;
